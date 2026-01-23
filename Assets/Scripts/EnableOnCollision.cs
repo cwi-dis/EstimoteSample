8b999d8fe5ee63880896841faeb73395be310e3a
VRTINY\dis <cwidisgroup@gmail.com>
2026-01-23 14:25:46 +0100

    Plug now turns on if dice is showing 3.

--- Assets/Scripts/EnableOnCollision.cs
+++ Assets/Scripts/EnableOnCollision.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnableOnCollision : MonoBehaviour
 {
-    public GameObject client;
+    public GameObject client_1;
+    public GameObject client_2;
+    public GameObject client_3;
+    public GameObject client_4;
+    public GameObject client_5;
+    public GameObject client_6;
     
     [SerializeField] public bool debug = true;
     // Start is called before the first frame update
@@ -13,20 +19,55 @@ public class EnableOnCollision : MonoBehaviour
       
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
     {
-        if (debug) Debug.Log($"Collision enter {gameObject.name}");
-        if (client != null)
+        Vector3 myUp = transform.up;
+        if (debug) Debug.Log($"Collision enter {gameObject.name} into {collision.gameObject.name}");
+        foreach (ContactPoint contact in collision.contacts)
         {
-            client.SetActive(true);
+            if (Vector3.Dot(myUp, Vector3.up) > 0.8f)
+            {
+                Debug.Log($"up, 1");
+                if (client_1 != null) client_1.SetActive(true);
+            } 
+            if (Vector3.Dot(myUp, Vector3.up) < -0.8f)
+            {
+                Debug.Log($"-up, 6");
+                if (client_6 != null) client_6.SetActive(true);
+            } 
+            if (Vector3.Dot(myUp, Vector3.right) > 0.8f) 
+            {
+                Debug.Log("right, 3");
+                if (client_3 != null) client_3.SetActive(true);
+                
+            } 
+            if (Vector3.Dot(myUp, Vector3.right) < -0.8f)
+            {
+                Debug.Log("-right, 4");
+                if (client_4 != null) client_4.SetActive(true);
+            } 
+            if (Vector3.Dot(myUp, Vector3.forward) > 0.8f)
+            {
+                Debug.Log("forward, 2");
+                if (client_2 != null) client_2.SetActive(true);
+            } 
+            if (Vector3.Dot(myUp, Vector3.forward) < -0.8f)
+            {
+                Debug.Log("-forward, 5");
+                if (client_5 != null) client_5.SetActive(true);
+            }
         }
     }
 
-    void onCollisionExit(Collision collision)
+    void OnCollisionExit(Collision collision)
     {
-        if (debug) Debug.Log($"Collision exit {gameObject.name}");
-        if (client != null) {
-            client.SetActive(false);}
+        if (debug) Debug.Log($"Collision exit {gameObject.name} from  {collision.gameObject.name}");
+        if (client_1 != null) client_1.SetActive(false);
+        if (client_2 != null) client_2.SetActive(false);
+        if (client_3 != null) client_3.SetActive(false);
+        if (client_4 != null) client_4.SetActive(false);
+        if (client_5 != null) client_5.SetActive(false);
+        if (client_6 != null) client_6.SetActive(false);
     }
     
     // Update is called once per frame
